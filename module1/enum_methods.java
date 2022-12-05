415f0e7550bf3757e2688f609403bdca73e5d6ec
shrsyc <shrsyc@gmail.com>
2022-12-05 23:37:53 +0530

    commented

--- module1/enum_methods.java
+++ module1/enum_methods.java
@@ -2,13 +2,13 @@ enum color{RED,WHITE,BLUE,GREEN,YELLOW}
 class colors{
     public static void main(String[] args) {
         color a=color.BLUE;
-        System.out.println(color.valueOf("BLUE"));
-        for(color c:color.values()){
-            System.out.println(c+" ordinal value is "+c.ordinal());
-            if(c.compareTo(a)<0)System.out.println(c+" comes before "+a);
-            if(c.compareTo(a)>0)System.out.println(c+" comes after "+a);
-            if(c.compareTo(a)==0)System.out.println(c+" equals "+a);
-            if(c.equals(a))System.out.println(c+" equals "+a);
+        System.out.println(color.valueOf("BLUE")); //------------------------------------> valueOf()             
+        for(color c:color.values()){//-------------------------------------------------------------->values()
+            System.out.println(c+" ordinal value is "+c.ordinal());//----------------------->ordinal()
+            if(c.compareTo(a)<0)System.out.println(c+" comes before "+a);//    --}
+            if(c.compareTo(a)>0)System.out.println(c+" comes after "+a);//     --}-->compareTo()
+            if(c.compareTo(a)==0)System.out.println(c+" equals "+a);//         --}
+            if(c.equals(a))System.out.println(c+" equals "+a);//---------------------->equals()
         }
     }
 }
