382c8a70db7c18bc9c94f213cc609af2695a0140
shrsyc <shrsyc@gmail.com>
2022-12-05 23:33:35 +0530

    added enum_methods

--- module1/enum_methods.java
+++ module1/enum_methods.java
@@ -1,1 +1,14 @@
-import java.util.*;
+enum color{RED,WHITE,BLUE,GREEN,YELLOW}
+class colors{
+    public static void main(String[] args) {
+        color a=color.BLUE;
+        System.out.println(color.valueOf("BLUE"));
+        for(color c:color.values()){
+            System.out.println(c+" ordinal value is "+c.ordinal());
+            if(c.compareTo(a)<0)System.out.println(c+" comes before "+a);
+            if(c.compareTo(a)>0)System.out.println(c+" comes after "+a);
+            if(c.compareTo(a)==0)System.out.println(c+" equals "+a);
+            if(c.equals(a))System.out.println(c+" equals "+a);
+        }
+    }
+}
