952a3857b8824f0e616ec904bc2c9974fe6efc49
shrsyc <shrsyc@gmail.com>
2022-12-06 23:03:52 +0530

    TabPane

--- module2/TabPane.java
+++ module2/TabPane.java
@@ -5,12 +5,33 @@ import javax.swing.*;
 class TabPane{
     TabPane(){
         JFrame jf=new JFrame("tabs");
-        jf.setSize(500,500);
+        jf.setSize(300,200);
         jf.setLayout(new FlowLayout());
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JTabbedPane jp=new JTabbedPane();
+        jp.setPreferredSize(new Dimension(200, 80));
+        JPanel p1=new JPanel();
+        JPanel p2=new JPanel();
+        JRadioButton b1=new JRadioButton("A");
+        JRadioButton b2=new JRadioButton("B");
+        ButtonGroup bg=new ButtonGroup();
+        bg.add(b1);
+        bg.add(b2);
+        p1.add(b2);
+        p1.add(b1);
+        JCheckBox c1=new JCheckBox("C++");
+        JCheckBox c2=new JCheckBox("JAVA");
+        p2.add(c2);
+        p2.add(c1);
+        jp.addTab("section", p1);
+        jp.addTab("languages", p2);
+        jf.add(jp);
         jf.setVisible(true);
     }
     public static void main(String[] args) {
-        new TabPane();
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run(){new TabPane();}  
+        });
+        
     }
 }
